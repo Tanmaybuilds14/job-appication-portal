4df2e7437b556034967e14b39009a49154edd2b5
Tanmaybuilds14 <sarvetanmay276@gmail.com>
2026-05-05 00:01:36 +0530

    minor fixes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://jobportal-seven-chi.vercel.app/register',
   headers: {
     'Content-Type': 'application/json',
   },
